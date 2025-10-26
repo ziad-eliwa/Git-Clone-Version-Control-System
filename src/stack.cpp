6049e5f2f2f88ee4c7b5066f63cd535f203cdf8d
aliahmed533 <aliahmed127279@gmail.com>
2025-10-26 21:36:38 +0300

    Added the stack class

---
+++ src/stack.cpp
@@ -0,0 +1,36 @@
+//
+// Created by Ali Ahmed  on 18/10/2025.
+//
+#include <iostream>
+#include "stack.h"
+
+using namespace std;
+template<class T>
+Stack <T>::Stack(int cap) {
+    capacity = cap;
+    arr = new int[cap];
+    top = -1;
+}
+template<class T>
+Stack <T>::~Stack() {
+
+}
+template<class T>
+bool Stack <T>::isEmpty() {
+    if (top==-1) return true;
+    else return false;
+}
+template<class T>
+bool Stack <T>::isFull() {
+    if (top==capacity-1) return true;
+    else return false;
+}
+template<class T>
+void Stack <T>::push(T x) {
+    arr[++top] = x;
+
+}
+template<class T>
+int Stack <T>::pop() {
+    return arr[top--];
+}
---
+++ src/stack.h
@@ -0,0 +1,25 @@
+//
+// Created by Ali Ahmed  on 18/10/2025.
+//
+
+#ifndef STACK_STACK_H
+#define STACK_STACK_H
+template <class T>
+
+class Stack {
+    int top;
+    int capacity;
+    int *arr;
+    public:
+    Stack(int);
+    ~Stack();
+    bool isEmpty();
+    bool isFull();
+    void push(T);
+    int pop();
+
+
+};
+
+
+#endif //STACK_STACK_H
