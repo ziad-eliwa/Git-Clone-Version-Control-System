5b55af163c3456d888788002ffb8f6774df43b47
pharaok <farouk.tamer03@gmail.com>
2025-11-17 22:05:05 +0200

    add support for positional arguments

--- src/argparser.cpp
+++ src/argparser.cpp
@@ -9,10 +9,12 @@ Option<T>::Option(T &data, std::string name, std::string description)
 
 // instatiate string and bool options
 template <> int Option<std::string>::parse(int argc, char *argv[]) {
-  if (argc < 2)
+  if (argc < 1)
     throw std::runtime_error("not enough arguments");
   if (argv[0] != name)
     return 0;
+  if (argc < 2)
+    throw std::runtime_error("not enough arguments");
   data = argv[1];
   return 2;
 }
@@ -23,6 +25,18 @@ template <> int Flag::parse(int argc, char *argv[]) {
     return 0;
   data = true;
   return 1;
+}
+
+IArgument::IArgument(std::string name, std::string description)
+    : name(name), description(description) {}
+template <class T>
+Argument<T>::Argument(T &data, std::string name, std::string description)
+    : IArgument(name, description), data(data) {}
+
+template <> void Argument<std::string>::parse(int argc, char *argv[]) {
+  if (argc < 1)
+    throw std::runtime_error("not enough arguments");
+  data = argv[0];
 }
 
 ArgParser::ArgParser()
@@ -38,6 +52,17 @@ ArgParser &ArgParser::add_command(std::string name, std::string description) {
   return *ap;
 }
 template <class T>
+ArgParser &ArgParser::add_argument(T &data, std::string name,
+                                   std::string description) {
+  IArgument *argument = new Argument(data, name, description);
+  arguments.push_back(argument);
+  return *this;
+}
+// instantiate add_argument for strings and bools
+template ArgParser &
+ArgParser::add_argument<std::string>(std::string &data, std::string name,
+                                     std::string description);
+template <class T>
 ArgParser &ArgParser::add_option(T &data, std::string name,
                                  std::string description) {
   IOption *option = new Option(data, name, description);
@@ -45,7 +70,7 @@ ArgParser &ArgParser::add_option(T &data, std::string name,
   return *this;
 }
 
-// instantiate add_option for strings and boold
+// instantiate add_option for strings and bools
 template ArgParser &ArgParser::add_option<std::string>(std::string &data,
                                                        std::string name,
                                                        std::string description);
@@ -58,7 +83,7 @@ ArgParser &ArgParser::set_callback(std::function<void()> cb) {
 }
 
 bool ArgParser::parse_args(int argc, char *argv[]) {
-  if (argc == 0)
+  if (argc < 0)
     return false;
   if (name != argv[0])
     return false;
@@ -69,6 +94,11 @@ bool ArgParser::parse_args(int argc, char *argv[]) {
   }
 
   int i = 1;
+  for (auto &arg : arguments) {
+    arg->parse(argc - i, (argv + i));
+    i++;
+  }
+
   while (i < argc) {
     bool anyMatched = false;
     for (auto opt : options) {
@@ -80,10 +110,17 @@ bool ArgParser::parse_args(int argc, char *argv[]) {
       }
     }
     if (!anyMatched)
-      break;
+      throw std::runtime_error(std::string("unrecognized option: ") + argv[i]);
   }
   // if (i < argc) // extra unparsed args
   if (callback != nullptr)
     callback();
   return true;
+}
+
+ArgParser::~ArgParser() {
+  for (int i = 0; i < commands.size(); i++)
+    delete commands[i];
+  for (int i = 0; i < options.size(); i++)
+    delete options[i];
 }
--- src/argparser.h
+++ src/argparser.h
@@ -22,20 +22,42 @@ public:
 };
 using Flag = Option<bool>;
 
-class ArgParser {
-private:
+class IArgument {
+public:
   std::string name;
   std::string description;
 
+  virtual ~IArgument() = default;
+  IArgument(std::string name, std::string description);
+  virtual void parse(int argc, char *argv[]) = 0;
+};
+template <class T> class Argument : public IArgument {
+private:
+  T &data;
+
+public:
+  Argument(T &data, std::string name, std::string description);
+  void parse(int argc, char *argv[]) override;
+};
+
+class ArgParser {
+private:
   Vector<ArgParser *> commands;
+  Vector<IArgument *> arguments;
   Vector<IOption *> options;
   std::function<void()> callback;
 
 public:
+  std::string name;
+  std::string description;
+
   ArgParser();
   ArgParser(std::string appName, std::string description);
+  ~ArgParser();
 
   ArgParser &add_command(std::string name, std::string description);
+  template <class T>
+  ArgParser &add_argument(T &data, std::string name, std::string description);
   template <class T>
   ArgParser &add_option(T &data, std::string name, std::string description);
   ArgParser &set_callback(std::function<void()> callback);
